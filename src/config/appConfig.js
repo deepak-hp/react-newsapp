9aa5c67134575dd68ef2e8f5abbe430e2e600c51
Deepak  HP <deepakhp19@gmail.com>
2021-09-20 21:45:25 +0530

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDfiNozIe2MJZJjm0Cp4arUTMfZ3FsvXzo",
-    authDomain: "newsapi-app-5853c.firebaseapp.com",
-    projectId: "newsapi-app-5853c",
-    storageBucket: "newsapi-app-5853c.appspot.com",
-    messagingSenderId: "266114857664",
-    appId: "1:266114857664:web:bedb60e9abcad0c99d8b25",
-    measurementId: "G-T7PL8QN1X8"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 export const newsApiKey = process.env.REACT_APP_NEWSAPI_APIKEY;
